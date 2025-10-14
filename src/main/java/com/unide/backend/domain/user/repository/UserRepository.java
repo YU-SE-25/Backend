9207fa325529ae8a7526efe7143257cbd900c3a6
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-10-14 12:46:37 +0900

    [추가] UserRepository에 existByEmail 쿼리 메서드 추가

--- src/main/java/com/unide/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/unide/backend/domain/user/repository/UserRepository.java
@@ -1,4 +1,4 @@
-// 사용자 정보를 데이터베이스에서 관리하기 위한 JPA 리포지토리 인터페이스
+// 사용자 정보를 데이터베이스에서 관리하기 위한 JPA 레포지토리 인터페이스
 
 package main.java.com.unide.backend.domain.user.repository;
 
@@ -6,5 +6,6 @@ import com.unide.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    // 이메일이 존재하는지 확인하는 메서드
+    boolean existsByEmail(String email);
 }
