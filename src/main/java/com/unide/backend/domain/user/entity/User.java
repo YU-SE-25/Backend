164934e365b97356fe3bf7d21728548792b66bbd
Woohyeon Roh <ykjrco@naver.com>
2025-10-16 01:19:07 +0900

    [추가] 라이브러리 임포트 구문 추가

--- src/main/java/com/unide/backend/domain/user/entity/User.java
+++ src/main/java/com/unide/backend/domain/user/entity/User.java
@@ -9,9 +9,12 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDateTime;
+import java.time.Duration;
 
 @Getter
 @Entity
