05c646bded3fdc3a7a1cc9fc8c7afc92c5782484
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-10-15 17:54:39 +0900

    [추가] AuthController에 이메일 인증 완료 API 추가

--- src/main/java/com/unide/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/unide/backend/domain/auth/controller/AuthController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.net.URI;
 
 import java.util.Map;
 
@@ -60,5 +63,12 @@ public class AuthController {
     public ResponseEntity<MessageResponseDto> sendVerificationEmail(@Valid @RequestBody EmailRequestDto requestDto) {
         authService.sendVerificationEmail(requestDto);
         return ResponseEntity.ok(new MessageResponseDto("인증 이메일이 발송되었습니다."));
+    }
+
+    @GetMapping("/email/verify-link")
+    public ResponseEntity<Void> verifyEmail(@RequestParam("token") String token) {
+        authService.verifyEmail(token);
+        URI redirectUri = URI.create("http://localhost:3000/login"); // 리다이렉트할 URL 
+        return ResponseEntity.status(HttpStatus.FOUND).location(redirectUri).build();
     }
 }
