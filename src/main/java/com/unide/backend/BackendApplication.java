9c8df50448640dd4067ec906d59f52c7e7bcdc99
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-10-22 06:32:38 +0900

    [환경] 파일 업로드 경로 설정 및 자동 생성 로직 추가

--- src/main/java/com/unide/backend/BackendApplication.java
+++ src/main/java/com/unide/backend/BackendApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+import java.beans.BeanProperty;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(@Value("${file.upload-dir}") String uploadDir) {
+        return args -> {
+            File dir = new File(uploadDir);
+            if (!dir.exists()) {
+                Files.createDirectories(Paths.get(uploadDir));
+                System.out.println("Created upload directory: " + uploadDir);
+            }
+        };
+    }
 }
