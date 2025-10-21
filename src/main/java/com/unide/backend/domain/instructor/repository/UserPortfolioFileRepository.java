1a1a2c574f9fc03a6f3456c5ed1f9169e01cec2b
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-10-22 08:16:20 +0900

    [추가] 포트폴리오 파일 관리를 위한 UserPortfolioFile 엔티티 및 Repository 추가

---
+++ src/main/java/com/unide/backend/domain/instructor/repository/UserPortfolioFileRepository.java
@@ -0,0 +1,10 @@
+// UserPortfolioFile 엔티티에 대한 데이터베이스 접근을 처리하는 JpaRepository
+
+package com.unide.backend.domain.instructor.repository;
+
+import com.unide.backend.domain.instructor.entity.UserPortfolioFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPortfolioFileRepository extends JpaRepository<UserPortfolioFile, Long> {
+
+}
