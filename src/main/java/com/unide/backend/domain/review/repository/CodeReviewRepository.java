f0b3030b22b5cc6486890fdaeeab66137baac99b
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-11-18 02:07:00 +0900

    [수정] 페이징 조회 메서드 수정

--- src/main/java/com/unide/backend/domain/review/repository/CodeReviewRepository.java
+++ src/main/java/com/unide/backend/domain/review/repository/CodeReviewRepository.java
@@ -4,10 +4,14 @@ package com.unide.backend.domain.review.repository;
 
 import com.unide.backend.domain.review.entity.CodeReview;
 import com.unide.backend.domain.submissions.entity.Submissions;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CodeReviewRepository extends JpaRepository<CodeReview, Long> {
-    Page<CodeReview> findBySubmissionOrderByCreatedAtDesc(Submissions submission, Pageable pageable);
+    @Query("SELECT cr FROM CodeReview cr JOIN FETCH cr.reviewer WHERE cr.submission = :submission ORDER BY cr.createdAt DESC")
+    Page<CodeReview> findBySubmission(@Param("submission") Submissions submission, Pageable pageable);
 }
