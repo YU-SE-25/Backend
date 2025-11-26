9d8052a8867339190c028cf44a0002fdb459e7d5
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-11-27 05:07:23 +0900

    [추가] 빠진 import 구문 추가

--- src/main/java/com/unide/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/unide/backend/domain/auth/controller/AuthController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.net.URI;
 import java.util.Map;
