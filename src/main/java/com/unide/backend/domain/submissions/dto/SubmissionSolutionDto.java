774da69093cc69deaa1c8ec4c88550beedc2f22c
jeong bo gyeong <bogyeong100499@naver.com>
2025-12-01 15:25:20 +0900

    [추가] 효율 추가 (#98)
    * [추가] 스터디그룹 추가
    
    * [추가]스터디그룹 crud
    
    * [추가]스터디그룹 문제 지정
    
    * [추가] studygroup에 가입, 강퇴, 퇴장...?(내가 나감)
    
    * [추가]메인페이지 최종
    
    * [추가]스터디그룹 로그
    
    * [추가] 스터디그룹 추가
    
    * [추가]스터디그룹 문제 지정
    
    * [추가] studygroup에 가입, 강퇴, 퇴장...?(내가 나감)
    
    * [추가]스터디그룹 로그
    
    # Conflicts:
    #	src/main/java/com/unide/backend/domain/studygroup/member/controller/StudyGroupMemberController.java
    #	src/main/java/com/unide/backend/domain/studygroup/member/entity/StudyGroupLog.java
    #	src/main/java/com/unide/backend/domain/studygroup/member/service/StudyGroupMemberService.java
    
    * [수정]오류 수정
    
    # Conflicts:
    #	src/main/java/com/unide/backend/domain/report/repository/ReportRepository.java
    
    * [추가] 토론 신고 추가
    
    * [추가] qna 신고 추가
    
    * [수정] 토론 게시판 신고 시 메시지 출력
    
    * fix: 스터디그룹 로그 충돌 해결
    
    * [수정]오류 수정
    
    * fix: mainpage 충돌 해결
    
    * [추가] review 신고
    
    * [수정] 메인화면 로그인없이 접근 가능
    
    * [수정] 게시물 및 댓글 올릴 때 author name 까지 뜸
    
    * [추가] submission 효율 순위 반영

--- src/main/java/com/unide/backend/domain/submissions/dto/SubmissionSolutionDto.java
+++ src/main/java/com/unide/backend/domain/submissions/dto/SubmissionSolutionDto.java
@@ -2,14 +2,21 @@
 
 package com.unide.backend.domain.submissions.dto;
 
-import com.unide.backend.domain.submissions.entity.SubmissionLanguage;
-import com.unide.backend.domain.submissions.entity.SubmissionStatus;
-import lombok.Builder;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
+import com.unide.backend.domain.submissions.entity.SubmissionLanguage;
+import com.unide.backend.domain.submissions.entity.SubmissionStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SubmissionSolutionDto {
     private Long submissionId;
@@ -20,4 +27,15 @@ public class SubmissionSolutionDto {
     private Integer runtime;
     private Integer memory;
     private LocalDateTime submittedAt;
+
+    
+    // ===== 효율 관련 추가 필드 =====
+    // 이 제출물의 리뷰 좋아요 총 개수
+    private Integer totalVotes;
+
+    // 효율 점수 (지금은 = totalVotes)
+    private Double efficiencyScore;
+
+    // 효율 순위 (1등, 2등, ...)
+    private Long efficiencyRank;
 }
