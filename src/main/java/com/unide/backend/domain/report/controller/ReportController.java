3a92fb65b690d3c6045d3facfadf12c9c76a4ffe
Woohyeon Roh <ykjrco@naver.com>
2025-12-04 17:03:18 +0900

    [추가] ReportController에 빠진 PutMapping과 PatchMapping import 구문 추가

--- src/main/java/com/unide/backend/domain/report/controller/ReportController.java
+++ src/main/java/com/unide/backend/domain/report/controller/ReportController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 import com.unide.backend.domain.report.dto.ReportCreateRequestDto;
 import com.unide.backend.domain.report.dto.ReportDetailDto;
